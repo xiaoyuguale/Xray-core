27018ec1fefbedfef457311676de03cd2211586a
liangyaoting <liangyaoting@casachina.com.cn>
2024-04-30 17:20:06 +0800

    commit_240430

--- main/main.go
+++ main/main.go
@@ -6,13 +6,20 @@ import (
 	"os"
 
 	"github.com/xtls/xray-core/main/commands/base"
-	_ "github.com/xtls/xray-core/main/distro/all"
+	_ "github.com/xtls/xray-core/main/distro/all" // 下划线表示仅执行package的init函数
 )
 
 func main() {
 	os.Args = getArgsV4Compatible()
 
+	// base.RootCommand是*base.Command类型的全局变量，定义在main/commands/base/root.go中
+	// 导入main/commands/base时，main/commands/base/root.go的init函数会实例化一个base.Command类型的结构体，把结构体指针赋值给base.RootCommand
+	// *base.Command.Commands是一个*base.Command类型变量的切片（子命令的实现方式？）
+	// 在main/distro/all中会导入了main/commands/all，而main/commands/all/commands.go的init函数会为base.RootCommand.Commands追加5个命令（api，tls，uuid，x25519，wg，追加之前Commands列表为空）
 	base.RootCommand.Long = "Xray is a platform for building proxies."
+	// 这里再在切片开头增加run和version命令
+	// 每个命令有对应的init函数，会对命令结构体的内容进行填充
+	// 最底层的命令会有对应的execute函数，在init阶段会赋值给命令结构体的Run字段，不是最底层命令，Commands切片填充支持的子命令
 	base.RootCommand.Commands = append(
 		[]*base.Command{
 			cmdRun,
@@ -26,12 +33,12 @@ func main() {
 func getArgsV4Compatible() []string {
 	// os.Args长度为1时，表示命令行仅有程序xray，设置默认command为run，并返回
 	if len(os.Args) == 1 {
-		fmt.Println("命令行仅有程序xray，设置默认command为run，返回xray run")
+		fmt.Println("提示：命令行仅有程序xray，设置默认command为run，返回xray run")
 		return []string{os.Args[0], "run"}
 	}
 	// os.Args[1]不以-开头，表示xray后指定了command，例如xray uuid，直接返回
 	if os.Args[1][0] != '-' {
-		fmt.Println("已为程序xray指定了command，直接返回")
+		fmt.Println("提示：已为程序xray指定了command，直接返回")
 		return os.Args
 	}
 	version := false
@@ -54,7 +61,7 @@ func getArgsV4Compatible() []string {
 		// fmt.Println("DEPRECATED: -h, WILL BE REMOVED IN V5.")
 		// fmt.Println("PLEASE USE: xray help")
 		// fmt.Println()
-		fmt.Println("检测到ErrHelp错误：解析到-h或者-help参数，但是未定义该flag，返回xray help")
+		fmt.Println("提示：检测到ErrHelp错误：解析到-h或者-help参数，但是未定义该flag，返回xray help")
 		return []string{os.Args[0], "help"}
 	}
 	// 解析到version为true，会进入到下面的代码，命令行需要是先解析到-version的形式：
@@ -66,7 +73,7 @@ func getArgsV4Compatible() []string {
 		// fmt.Println("DEPRECATED: -version, WILL BE REMOVED IN V5.")
 		// fmt.Println("PLEASE USE: xray version")
 		// fmt.Println()
-		fmt.Println("解析到-version or --version or -version=true，返回xray version")
+		fmt.Println("提示：解析到-version or --version or -version=true，返回xray version")
 		return []string{os.Args[0], "version"}
 	}
 	// fmt.Println("COMPATIBLE MODE, DEPRECATED.")
@@ -75,7 +82,7 @@ func getArgsV4Compatible() []string {
 	// 除了上面的情况，其他情况都会执行下面的代码，例如：
 	// 1. xray -run -h
 	// 2. xray -run -version
-	fmt.Println("命令行参数格式错误，设置默认command为run，返回其他命令行参数")
+	fmt.Println("提示：命令行参数格式错误，设置默认command为run，返回其他命令行参数")
 	return append([]string{os.Args[0], "run"}, os.Args[1:]...)
 }
 
