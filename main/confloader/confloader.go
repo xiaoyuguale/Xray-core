7d228aaddf2f4ed18533b8b40eb556739c341af0
xiaoyuguale <717116926@qq.com>
2024-06-23 07:30:04 +0000

    commit_240623

--- main/confloader/confloader.go
+++ main/confloader/confloader.go
@@ -19,9 +19,11 @@ var (
 // LoadConfig reads from a path/url/stdin
 // actual work is in external module
 func LoadConfig(file string) (io.Reader, error) {
+	// EffectiveConfigFileLoader在main/confloader/external/external.go的init函数被赋值了ConfigLoader函数，跳转ConfigLoader的定义
 	if EffectiveConfigFileLoader == nil {
 		errors.LogInfo(context.Background(), "external config module not loaded, reading from stdin")
 		return os.Stdin, nil
 	}
+	// 调用EffectiveConfigFileLoader相当于调用ConfigLoader
 	return EffectiveConfigFileLoader(file)
 }
