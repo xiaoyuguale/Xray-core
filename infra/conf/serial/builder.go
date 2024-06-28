4a822f60c43c084b9fffa315848474dd3e17ed38
liangyaoting <liangyaoting@axyomcorechina.com>
2024-06-28 14:12:06 +0800

    commit_240628

--- infra/conf/serial/builder.go
+++ infra/conf/serial/builder.go
@@ -10,11 +10,13 @@ import (
 )
 
 func MergeConfigFromFiles(files []string, formats []string) (string, error) {
+	// 这里返回一个合并好的*conf.Config对象
 	c, err := mergeConfigs(files, formats)
 	if err != nil {
 		return "", err
 	}
 
+	// 对Config对象进行序列化，跳转common/reflect/marshal.go查看MarshalToJson的定义
 	if j, ok := creflect.MarshalToJson(c); ok {
 		return j, nil
 	}
@@ -37,12 +39,15 @@ func mergeConfigs(files []string, formats []string) (*conf.Config, error) {
 		if err != nil {
 			return nil, newError("failed to decode config: ", file).Base(err)
 		}
+		// i==0表示解析第一个文件，直接把返回的Config对象保存到cf
 		if i == 0 {
 			*cf = *c
 			continue
 		}
+		// 后续每解析一个文件，都会和cf进行合并，跳转infra/conf/xray.go查看Override的定义
 		cf.Override(c, file)
 	}
+	// 返回合并好的Config对象
 	return cf, nil
 }
 
